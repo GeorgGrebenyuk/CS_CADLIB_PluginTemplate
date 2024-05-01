9d69973b4f95555780c0d23be8f732b33f99e4bf
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2024-05-01 11:59:39 +0300

    upload raw project

---
+++ UI/UI_PluginStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CADLib_Plugin_UI
+{
+    public partial class UI_PluginStart : Form
+    {
+        public UI_PluginStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Loader/CADLibPluginEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using CADLib;
+using CADLib_Plugin_UI;
+
+namespace CADLib_PluginTemplate
+{
+    public class CADLibPluginEntryPoint
+    {
+        /// <summary>
+        /// Статический метод регистрации плагин, вызываемый родительским приложением
+        /// </summary>
+        /// <param name="manager">Объект текущего окружения плагина</param>
+        /// <returns>Интерфейс плагина</returns>
+        public static ICADLibPlugin RegisterPlugin(PluginsManager manager)
+        {
+            //check autorization
+            return new UI_PluginStart(manager);
+        }
+
+    }
+}
